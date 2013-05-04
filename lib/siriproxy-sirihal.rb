6b2cc9f4c1ca5f4ebbc5058f437cdb97f1a078d2
dtomcat <dtomcat@hotmail.com>
2013-05-03 20:37:42 -0500

    Update siriproxy-sirihal.rb

--- lib/siriproxy-sirihal.rb
+++ lib/siriproxy-sirihal.rb
@@ -119,13 +119,13 @@ class SiriProxy::Plugin::SiriHal < SiriProxy::Plugin
 	listen_for(/What is the(?:current )? temperature in the house/i) do
 		send_temp_house("STAT",self.therm,"GetTemp","0")
 	end
-	listen_for(/What is the(?:current )? temperature of(?:the )? (.*)/i) do |qDevice|
+	listen_for(/What is the(?:current )? temperature of the (.*)/i) do |qDevice|
 		send_temp_house("STAT",qDevice,"GetTemp","0")
 	end
-	listen_for(/What is the(?:current )? mode of(?:the )? (.*)/i) do |qDevice|
+	listen_for(/What is the(?:current )? mode of the (.*)/i) do |qDevice|
 		send_temp_house("STAT",qDevice,"GetMode","0")
 	end
-	listen_for(/Set(?:the )? mode of(?:the )? (.*) to (.*) mode/i) do |qDevice,qMode|
+	listen_for(/Set(?:the )? mode of the (.*) to (.*) mode/i) do |qDevice,qMode|
 		send_temp_house("STAT",qDevice,"SetMode",qMode)
 	end
 	
