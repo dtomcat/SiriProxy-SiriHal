2c5293bae47d88b61351ea4b88fe9750324c414f
dtomcat <dtomcat@hotmail.com>
2013-05-03 19:50:05 -0500

    Update siriproxy-sirihal.rb
    Send "0" instead of 0

--- lib/siriproxy-sirihal.rb
+++ lib/siriproxy-sirihal.rb
@@ -117,10 +117,10 @@ class SiriProxy::Plugin::SiriHal < SiriProxy::Plugin
 	end
 	#HVAC Commands
 	listen_for(/What is the current temperature in the house/i) do
-		send_temp_house("STAT",self.therm,"GetTemp",0)
+		send_temp_house("STAT",self.therm,"GetTemp","0")
 	end
 	listen_for(/What is the temperature of the (.*)/i) do |qDevice|
-		send_to_house("STAT",qDevice,"GetTemp",0)
+		send_to_house("STAT",qDevice,"GetTemp","0")
 	end
 	#Shutdown Command
 	listen_for(/Shutdown the Siri server/i) do
